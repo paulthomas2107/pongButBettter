8957d618a9969f0dea794085ed477c3a4a40b172
Paul Thomas <paulthomas2107@gmail.com>
2023-08-23 17:25:11 +0100

    More

--- pongBytBetter.py
+++ pongBytBetter.py
@@ -21,6 +21,7 @@ ball_vel_x, ball_vel_y = 0.7, 0.7
 paddle_width, paddle_height = 20, 120
 left_paddle_y = right_paddle_y = HEIGHT / 2 - paddle_height / 2
 left_paddle_x, right_paddle_x = 100 - paddle_width / 2, WIDTH - (100 - paddle_width / 2)
+right_paddle_vel = left_paddle_vel = 0
 
 # Main loop
 while run:
@@ -28,6 +29,19 @@ while run:
     for i in pygame.event.get():
         if i.type == pygame.QUIT:
             run = False
+        elif i.type == pygame.KEYDOWN:
+            if i.key == pygame.K_UP:
+                right_paddle_vel = -0.9
+            if i.key == pygame.K_DOWN:
+                right_paddle_vel = 0.9
+            if i.key == pygame.K_w:
+                left_paddle_vel = -0.9
+            if i.key == pygame.K_s:
+                left_paddle_vel = 0.9
+
+        if i.type == pygame.KEYUP:
+            right_paddle_vel = 0
+            left_paddle_vel = 0
 
     # Ball movement
     if ball_y <= 0 + radius or ball_y >= HEIGHT - radius:
@@ -43,6 +57,8 @@ while run:
     # Movement
     ball_x += ball_vel_x
     ball_y += ball_vel_y
+    right_paddle_y += right_paddle_vel
+    left_paddle_y += left_paddle_vel
     # Objects
     pygame.draw.circle(wn, BLUE, (ball_x, ball_y), radius)
     pygame.draw.rect(wn, RED, pygame.Rect(left_paddle_x, left_paddle_y, paddle_width, paddle_height))
