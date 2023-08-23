643addbc183a6c55539338f6bf7f726e5a7d47ab
Paul Thomas <paulthomas2107@gmail.com>
2023-08-23 16:55:14 +0100

    More

--- pongBytBetter.py
+++ pongBytBetter.py
@@ -11,11 +11,12 @@ run = True
 # Colors
 BLUE = (0, 0, 255)
 RED = (255, 0, 0)
+BLACK = (0, 0, 0)
 
 # Ball
 radius = 15
 ball_x, ball_y = WIDTH / 2 - radius, HEIGHT / 2 - radius
-
+ball_vel_x, ball_vel_y = 0.7, 0.7
 # Paddle Dimensions
 paddle_width, paddle_height = 20, 120
 left_paddle_y = right_paddle_y = HEIGHT / 2 - paddle_height / 2
@@ -23,10 +24,26 @@ left_paddle_x, right_paddle_x = 100 - paddle_width / 2, WIDTH - (100 - paddle_wi
 
 # Main loop
 while run:
+    wn.fill(BLACK)
     for i in pygame.event.get():
         if i.type == pygame.QUIT:
             run = False
 
+    # Ball movement
+    if ball_y <= 0 + radius or ball_y >= HEIGHT - radius:
+        ball_vel_y *= -1
+    if ball_x >= WIDTH - radius:
+        ball_x, ball_y = WIDTH / 2 - radius, HEIGHT / 2 - radius
+        ball_vel_x *= -1
+        ball_vel_y *= -1
+    if ball_x <= 0 + radius:
+        ball_x, ball_y = WIDTH / 2 - radius, HEIGHT / 2 - radius
+        ball_vel_x, ball_vel_y = 0.7, 0.7
+
+    # Movement
+    ball_x += ball_vel_x
+    ball_y += ball_vel_y
+    # Objects
     pygame.draw.circle(wn, BLUE, (ball_x, ball_y), radius)
     pygame.draw.rect(wn, RED, pygame.Rect(left_paddle_x, left_paddle_y, paddle_width, paddle_height))
     pygame.draw.rect(wn, RED, pygame.Rect(right_paddle_x, right_paddle_y, paddle_width, paddle_height))
